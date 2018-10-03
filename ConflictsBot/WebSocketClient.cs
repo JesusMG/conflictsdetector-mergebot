49a9bd69a2afaba249233ebd66b3acad9e918558
jesus <>
2018-10-04 00:12:06 +0200

    add WebSocketClient

---
+++ ConflictsBot/WebSocketClient.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Threading;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+
+using Newtonsoft.Json.Linq;
+
+namespace ConflictsBot
+{
+    class WebSocketClient
+    {
+        internal WebSocketClient(
+            string serverUrl,
+            string name,
+            string apikey,
+            Action<string> processMessage)
+        {
+            mName = name;
+            mApiKey = apikey;
+
+            mCancelToken = mCancelTokenSource.Token;
+
+            mWebSocket = new ClientWebSocket();
+            mWebSocket.Options.RemoteCertificateValidationCallback += CertificateValidation;
+            mWebSocket.Options.KeepAliveInterval = TimeSpan.FromSeconds(20);
+            mUri = new Uri(serverUrl);
+
+            mProcessMessage = processMessage;
+        }
+		
+        async void ConnectAsync()
+        {
+            await mWebSocket.ConnectAsync(mUri, mCancelToken);
+            SendMessage(BuildLoginMessage(mApiKey));
+            SendMessage(BuildRegisterTriggerMessage(BRANCH_ATTRIBUTE_CHANGED_TRIGGER_TYPE));
+
+            mLog.InfoFormat("ConflictsBot [{0}] connected!", mName);
+
+            StartListen();
+        }
+
+        static string BuildLoginMessage(string token)
+        {
+            JObject obj = new JObject(
+                new JProperty("action", "login"),
+                new JProperty("key", token));
+
+            return obj.ToString();
+        }
+
+        static string BuildRegisterTriggerMessage(params string[] triggers)
+        {
+            JObject obj = new JObject(
+                new JProperty("action", "register"),
+                new JProperty("type", "trigger"),
+                new JProperty("eventlist", new JArray(triggers)));
+
+            return obj.ToString();
+        }
+
+        async void StartListen()
+        {
+            var buffer = new byte[ReceiveChunkSize];
+
+            try
+            {
+                while (mWebSocket.State == WebSocketState.Open)
+                {
+                    var stringResult = new StringBuilder();
+
+
+                    WebSocketReceiveResult result;
+                    do
+                    {
+                        result = await mWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), mCancelToken);
+
+                        if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            await
+                                mWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                        }
+                        else
+                        {
+                            var str = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                            stringResult.Append(str);
+                        }
+
+                    } while (!result.EndOfMessage);
+
+                    CallOnMessage(stringResult);
+
+                }
+            }
+            catch (Exception e)
+            {
+                mLog.ErrorFormat("StartListen failed: {0}", e.Message);
+            }
+            finally
+            {
+                mWebSocket.Dispose();
+            }
+        }
+
+        void CallOnMessage(StringBuilder stringResult)
+        {
+            if (mProcessMessage != null)
+                RunInTask(() => mProcessMessage(stringResult.ToString()));
+        }
+
+        static void RunInTask(Action action)
+        {
+            Task.Factory.StartNew(action);
+        }
+
+        void SendMessage(string message)
+        {
+            SendMessageAsync(message);
+        }
+
+        async void SendMessageAsync(string message)
+        {
+            if (mWebSocket.State != WebSocketState.Open)
+            {
+                throw new Exception("Connection is not open.");
+            }
+
+            var messageBuffer = Encoding.UTF8.GetBytes(message);
+            var messagesCount = (int)Math.Ceiling((double)messageBuffer.Length / SendChunkSize);
+
+            for (var i = 0; i < messagesCount; i++)
+            {
+                var offset = (SendChunkSize * i);
+                var count = SendChunkSize;
+                var lastMessage = ((i + 1) == messagesCount);
+
+                if ((count * (i + 1)) > messageBuffer.Length)
+                {
+                    count = messageBuffer.Length - offset;
+                }
+
+                await mWebSocket.SendAsync(
+                    new ArraySegment<byte>(messageBuffer, offset, count), 
+                    WebSocketMessageType.Text, 
+                    lastMessage, 
+                    mCancelToken);
+            }
+        }
+
+        static bool CertificateValidation(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        readonly ClientWebSocket mWebSocket;
+
+        readonly Uri mUri;
+
+        readonly CancellationToken mCancelToken;
+        readonly CancellationTokenSource mCancelTokenSource = new CancellationTokenSource();
+
+        readonly string mName;
+        readonly string mApiKey;
+        readonly Action<string> mProcessMessage;
+
+        const int ReceiveChunkSize = 1024;
+        const int SendChunkSize = 1024;
+
+        const string BRANCH_ATTRIBUTE_CHANGED_TRIGGER_TYPE = "branchAttributeChanged";
+        
+        static readonly ILog mLog = LogManager.GetLogger(typeof(WebSocketClient));
+    }
+}
